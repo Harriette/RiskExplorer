081aee00497b9efaf904d8eca2d1477e4a723f26
Harriette <nigel.desilvafamily@gmail.com>
2020-12-29 17:02:40 +0000

    Initial templates

---
+++ src/index.php
@@ -0,0 +1,21 @@
+<?php
+  session_start();
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Risk Explorer</title>
+<?php require_once "head.php" ?>
+  </head>
+  <body>
+    <div class="">
+      <h1>Risk Explorer</h1>
+    </div>
+  </body>
+</html>
