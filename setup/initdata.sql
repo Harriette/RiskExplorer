dda0f96ec8bc8a036f6f141db3605170c5704a99
Harriette <nigel.desilvafamily@gmail.com>
2020-12-30 16:41:49 +0000

    Established MySQL database

---
+++ setup/initdata.sql
@@ -0,0 +1,46 @@
+INSERT INTO companies (name)
+  VALUES
+    ('Choose Company'),
+    ('UK Club'),
+    ('TM');
+
+INSERT INTO departments (name)
+  VALUES
+    ('Choose Department'),
+    ('Claims'),
+    ('Underwriting'),
+    ('Management'),
+    ('Finance'),
+    ('Loss Prevention'),
+    ('Actuarial');
+
+INSERT INTO profiles (first_name, last_name, company_ID, department_ID)
+  VALUES
+    ('Nigel', 'De Silva', 2, 4);
+
+INSERT INTO users (user_name, password_hash, profile_ID)
+  VALUES
+    ('desilvan', '123', 1);
+
+INSERT INTO processes (name)
+  VALUES
+    ('Choose Process'),
+    ('Planning'),
+    ('Governance'),
+    ('Resourcing'),
+    ('Systems/processes'),
+    ('Data'),
+    ('Work'),
+    ('Communication');
+
+INSERT INTO risks (id, name, description, loss, risk_level, company_ID, department_ID, process_ID)
+  VALUES
+    ('P1','Premium', 'Premium different to expectations.', 1, 1, 2, 1, 1),
+    ('P2','Premium volumes', 'Premium volume different to expectations.', 1, 2, 2, 1, 1),
+    ('P3','Premium adequacy', 'Premium adequacy different to expectations.', 1, 2, 2, 1, 1),
+    ('C1','Claims', 'Claims different to expectations.', 1, 1, 2, 1, 1);
+
+INSERT INTO risklinks (riskfrom_ID, riskto_ID)
+  VALUES
+    (2, 1),
+    (2, 1);
---
+++ setup/initdb.sql
@@ -0,0 +1,101 @@
+DROP DATABASE IF EXISTS riskexplorer;
+CREATE DATABASE riskexplorer;
+USE riskexplorer;
+
+CREATE TABLE IF NOT EXISTS companies (
+  company_ID      INT UNSIGNED  NOT NULL AUTO_INCREMENT,
+  name            VARCHAR(128)  NOT NULL DEFAULT '',
+  PRIMARY KEY     (company_ID)
+);
+
+CREATE TABLE IF NOT EXISTS departments (
+  department_ID   INT UNSIGNED  NOT NULL AUTO_INCREMENT,
+  name            VARCHAR(64)   NOT NULL DEFAULT '',
+  PRIMARY KEY     (department_ID)
+);
+
+CREATE TABLE IF NOT EXISTS profiles (
+  profile_ID      INT UNSIGNED  NOT NULL AUTO_INCREMENT,
+  first_name      VARCHAR(128)  NOT NULL DEFAULT '',
+  last_name       VARCHAR(128)  NOT NULL DEFAULT '',
+  company_ID	  INT UNSIGNED,
+  department_ID	  INT UNSIGNED,
+  picture_path	  VARCHAR(512),		
+  
+  PRIMARY KEY     (profile_ID),
+  INDEX           (first_name, last_name),
+  
+  CONSTRAINT FOREIGN KEY (company_ID)
+    REFERENCES companies (company_ID),
+  CONSTRAINT FOREIGN KEY (department_ID)
+    REFERENCES departments (department_ID)
+);
+
+CREATE TABLE IF NOT EXISTS users (
+  user_ID      	  INT UNSIGNED  NOT NULL AUTO_INCREMENT,
+  user_name       VARCHAR(128)  NOT NULL DEFAULT '',
+  password_hash   VARCHAR(128)  NOT NULL DEFAULT '',
+  profile_ID		  INT UNSIGNED,
+  
+  PRIMARY KEY     (user_ID),
+  INDEX           (user_name),
+  
+  CONSTRAINT FOREIGN KEY (profile_ID)
+  REFERENCES profiles (profile_ID)
+);
+
+CREATE TABLE IF NOT EXISTS processes (
+  process_ID      INT UNSIGNED  NOT NULL AUTO_INCREMENT,
+  name            VARCHAR(64)   NOT NULL DEFAULT '',
+  PRIMARY KEY     (process_ID)
+);
+
+CREATE TABLE IF NOT EXISTS risks (
+  risk_ID           INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  id                VARCHAR(10)   NOT NULL DEFAULT '',
+  name              VARCHAR(128)   NOT NULL DEFAULT '',
+  description       VARCHAR(1024),
+  prob_rating       TINYINT,
+  severity_rating   TINYINT,
+  reputation_rating TINYINT,
+  rag_rating        TINYINT,
+  loss              BOOLEAN       DEFAULT 0,
+  risk_level        TINYINT,
+  company_ID           INT UNSIGNED  NOT NULL,
+  department_ID     INT UNSIGNED  NOT NULL,
+  process_ID        INT UNSIGNED  NOT NULL,
+  created_at        TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  created_by        VARCHAR(50),
+  modified_at       TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  modified_by       VARCHAR(50),
+  is_deleted        BOOLEAN DEFAULT 0,
+
+  PRIMARY KEY       (risk_ID),
+  INDEX             (name),
+
+  CONSTRAINT FOREIGN KEY (company_ID)
+    REFERENCES companies (company_ID),
+  CONSTRAINT FOREIGN KEY (department_ID)
+    REFERENCES departments (department_ID),
+  CONSTRAINT FOREIGN KEY (process_ID)
+    REFERENCES processes (process_ID)
+);
+
+
+CREATE TABLE IF NOT EXISTS risklinks (
+  risklink_ID     INT UNSIGNED   NOT NULL AUTO_INCREMENT,
+  riskfrom_ID     INT UNSIGNED   NOT NULL,
+  riskto_ID       INT UNSIGNED   NOT NULL,
+
+  PRIMARY KEY     (risklink_ID),
+
+  CONSTRAINT FOREIGN KEY (riskfrom_ID)
+    REFERENCES risks (risk_ID)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+
+  CONSTRAINT FOREIGN KEY (riskto_ID)
+    REFERENCES risks (risk_ID)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
